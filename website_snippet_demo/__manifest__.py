9c469cbde96dc44b25ced7b956ed3e6eabf0a444
Krishna Gautam <168166801+Krishna2082g@users.noreply.github.com>
2025-06-10 17:55:56 +0545

    ALL code day 1

---
+++ website_snippet_demo/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Website Snippet Demo",
+    "category": "Website",
+    "summary": "Custom snippets for website",
+    "version": "1.0",
+    "depends": ["website"],
+    "data": [
+        "views/custom_snippet_templates.xml",
+        "views/custom_snippet.xml",
+    ],
+    "installable": True,
+    "application": True,
+}
